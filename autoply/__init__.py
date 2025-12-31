7a4b164676e5a1a003e4e4352c3b4a3d96c7d52a
Gavtriple9 <29155514+Gavtriple9@users.noreply.github.com>
2025-12-30 21:04:51 -0500

    restart

--- autoply/__init__.py
+++
@@ -1,9 +0,0 @@
-import os
-
-THIS_FILE = os.path.abspath(__file__)
-ROOT_DIR = os.path.dirname(os.path.dirname(THIS_FILE))
-ROOT_LOGGER_NAME = __name__.lower()
-
-from autoply.scraper import JobScraper, Site
-from autoply.logger import get_root_logger
-from autoply.parser import get_parser
--- autoply/scrapers/consts.py
+++
@@ -1,1 +0,0 @@
-USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36"
